3a91c9e52256e1049d4f9aec233c06a472f20841
ranked001 <ranked001@BeAsT.(none)>
2014-01-22 18:03:04 +0000

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Sun Jan 5 16:51:23 GMT 2014"
+#define LINUX_COMPILE_BY "ranked001"
+#define LINUX_COMPILE_HOST "BeAsT"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.8.2 20130603 (prerelease) (Linaro GCC 4.8-2013.06-0~dev) "
