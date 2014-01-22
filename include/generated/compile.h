90e181875f11840e47459816bc137c06d8b0bd8b
ranked001 <ranked001@BeAsT.(none)>
2014-01-22 19:32:40 +0000

    ion support

---
+++ include/generated/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Fri Jan 3 00:39:39 GMT 2014"
+#define LINUX_COMPILE_BY "ranked001"
+#define LINUX_COMPILE_HOST "BeAsT"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.8.2 20130603 (prerelease) (Linaro GCC 4.8-2013.06-0~dev) "
